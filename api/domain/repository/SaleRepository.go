ef12f2d045804ebcfbfaa0a078714cff46d38a98
realnfcs <nicolas.cavalcante.dev@gmail.com>
2022-07-14 15:23:10 -0400

    [Alpha ⚠] Adição do usecase GetSale, seus métodos e fluxo lógicos

--- api/domain/repository/SaleRepository.go
+++ api/domain/repository/SaleRepository.go
@@ -5,6 +5,7 @@ import "github.com/realnfcs/ultividros-project/api/domain/entities"
 // A interface SaleRepository provém os métodos necessários para
 // que o repositório a ser usado realiza o objetivo do usecase
 type SaleRepository interface {
+	GetSale(string) (*entities.Sale, int, error)
 	GetSales() (*[]entities.Sale, int, error)
-	SaveSale(entities.Sale /*, []entities.PartsReq, []entities.CommonGlssReq, []entities.TempGlssReq */) (string, int, error)
+	SaveSale(entities.Sale) (string, int, error)
 }
---
+++ api/domain/usecases/sales/getsale/Output.go
@@ -0,0 +1,124 @@
+package getsale
+
+import (
+	"sync"
+
+	"github.com/realnfcs/ultividros-project/api/domain/entities"
+)
+
+// Usecase Output Port responsável pelos dados que serão retornados
+type Output struct {
+	OutputData `json:"data"`
+	Status     int    `json:"-"`
+	Err        string `json:"error"`
+}
+
+// OutpurData responsável pelo dado da entity em si que será passado pelas camadas externas
+type OutputData struct {
+	Id            string          `json:"id"`
+	ClientId      string          `json:"client_id"`
+	CommonGlssReq []CommonGlssReq `json:"common_glss_req"`
+	PartsReq      []PartsReq      `json:"parts_req"`
+	TempGlssReq   []TempGlssReq   `json:"temp_glss"`
+}
+
+// Informações contidas dentro do OutputData
+type ProductsRequest struct {
+	Id           string  `json:"id"`
+	ProductId    string  `json:"product_id"`
+	ProductName  string  `json:"product_name"`
+	ProductPrice float32 `json:"product_price"`
+	ProdtQtyReq  uint32  `json:"prodt_qty_req"`
+	SaleId       string  `json:"sale_id"`
+}
+
+type TempGlssReq struct {
+	ProductsRequest
+}
+
+type PartsReq struct {
+	ProductsRequest
+}
+
+type CommonGlssReq struct {
+	ProductsRequest
+	RequestWidth  float32 `json:"request_width"`
+	RequestHeight float32 `json:"request_height"`
+}
+
+func (*Output) Init(e *entities.Sale, status int, err error) *Output {
+	if e != nil {
+
+		var wg sync.WaitGroup
+
+		comnGlssReq := make([]CommonGlssReq, len(e.CommonGlssReq))
+
+		if len(e.CommonGlssReq) > 0 {
+			wg.Add(1)
+
+			go func() {
+				for i, v := range e.CommonGlssReq {
+					comnGlssReq[i].Id = v.Id
+					comnGlssReq[i].ProductId = v.ProductId
+					comnGlssReq[i].ProductName = v.ProductName
+					comnGlssReq[i].ProductPrice = v.ProductPrice
+					comnGlssReq[i].ProdtQtyReq = v.ProdtQtyReq
+					comnGlssReq[i].RequestWidth = v.RequestWidth
+					comnGlssReq[i].RequestHeight = v.RequestHeight
+					comnGlssReq[i].SaleId = v.SaleId
+				}
+				wg.Done()
+			}()
+		}
+
+		partReq := make([]PartsReq, len(e.PartsReq))
+
+		if len(e.PartsReq) > 0 {
+			wg.Add(1)
+
+			go func() {
+				for i, v := range e.PartsReq {
+					partReq[i].Id = v.Id
+					partReq[i].ProductId = v.ProductId
+					partReq[i].ProductName = v.ProductName
+					partReq[i].ProductPrice = v.ProductPrice
+					partReq[i].ProdtQtyReq = v.ProdtQtyReq
+					partReq[i].SaleId = v.SaleId
+				}
+				wg.Done()
+			}()
+		}
+
+		tempGlssReq := make([]TempGlssReq, len(e.TempGlssReq))
+
+		if len(e.TempGlssReq) > 0 {
+			wg.Add(1)
+
+			go func() {
+				for i, v := range e.TempGlssReq {
+					tempGlssReq[i].Id = v.Id
+					tempGlssReq[i].ProductId = v.ProductId
+					tempGlssReq[i].ProductName = v.ProductName
+					tempGlssReq[i].ProductPrice = v.ProductPrice
+					tempGlssReq[i].ProdtQtyReq = v.ProdtQtyReq
+					tempGlssReq[i].SaleId = v.SaleId
+				}
+				wg.Done()
+			}()
+		}
+
+		return &Output{
+			OutputData: OutputData{
+				Id:            e.Id,
+				ClientId:      e.ClientId,
+				CommonGlssReq: comnGlssReq,
+				PartsReq:      partReq,
+				TempGlssReq:   tempGlssReq,
+			},
+			Status: status,
+			Err:    "",
+		}
+	}
+
+	return &Output{OutputData{}, status, err.Error()}
+}
